71ef4369dd9e786468233aa69c72fc4fd30a631a
Zac Hadjineophytou <zac1999ad@gmail.com>
2019-08-07 16:36:21 +0100

    Created project with nav drawer

---
+++ app/src/main/java/uk/co/zac_h/spacex/base/MainActivity.kt
@@ -0,0 +1,30 @@
+package uk.co.zac_h.spacex.base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
+import uk.co.zac_h.spacex.R
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host)
+
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val appBarConfig = AppBarConfiguration(navController.graph, drawerLayout)
+
+        findViewById<Toolbar>(R.id.toolbar).setupWithNavController(navController, appBarConfig)
+        findViewById<NavigationView>(R.id.nav_view).setupWithNavController(navController)
+
+    }
+
+}
---
+++ app/src/main/java/uk/co/zac_h/spacex/dashboard/DashboardFragment.kt
@@ -0,0 +1,32 @@
+package uk.co.zac_h.spacex.dashboard
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import uk.co.zac_h.spacex.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class DashboardFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    }
+
+
+}
