2c7d8aa77e819b88eae21e7da61f651053ca0aaf
Zac Hadjineophytou <zac1999ad@gmail.com>
2019-08-30 21:28:46 +0100

    Fixed payload weight showing as null

--- app/src/main/java/uk/co/zac_h/spacex/launches/adapters/PayloadAdapter.kt
+++ app/src/main/java/uk/co/zac_h/spacex/launches/adapters/PayloadAdapter.kt
@@ -11,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView
 import uk.co.zac_h.spacex.R
 import uk.co.zac_h.spacex.utils.data.PayloadModel
 import uk.co.zac_h.spacex.utils.formatCustomers
+import kotlin.math.roundToInt
 
 class PayloadAdapter(private var context: Context?, private var payloads: List<PayloadModel>?) :
     RecyclerView.Adapter<PayloadAdapter.ViewHolder>() {
@@ -45,8 +46,8 @@ class PayloadAdapter(private var context: Context?, private var payloads: List<P
             payloadLifespan.text = payload?.orbitParams?.lifespanYears?.toInt()?.toString() ?: "0"
             payloadMass.text = context?.getString(
                 R.string.payload_value,
-                payload?.massKg?.toInt(),
-                payload?.massLbs?.toInt()
+                payload?.massKg?.roundToInt() ?: 0,
+                payload?.massLbs?.roundToInt() ?: 0
             )
             payloadType.text = payload?.type ?: "Unknown"
             payloadRefSystem.text = payload?.orbitParams?.referenceSystem ?: "Unknown"
