e1b19047dbbf8711f002dbf4e9c6414677fbaafb
Zac Hadjineophytou <zac1999ad@gmail.com>
2019-09-03 15:55:57 +0100

    Added Wear App and Watch Face modules

---
+++ wear/src/main/java/uk/co/zac_h/spacex/MainActivity.kt
@@ -0,0 +1,15 @@
+package uk.co.zac_h.spacex
+
+import android.os.Bundle
+import android.support.wearable.activity.WearableActivity
+
+class MainActivity : WearableActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Enables Always-on
+        setAmbientEnabled()
+    }
+}
