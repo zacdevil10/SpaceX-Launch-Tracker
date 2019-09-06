727ea42173b265f0431cea66b9d3c8eeed890f26
Zac Hadjineophytou <zac1999ad@gmail.com>
2019-09-06 17:47:54 +0100

    Refactored Wear Dashboard

--- wear/src/main/java/uk/co/zac_h/spacex/dashboard/DashboardWearFragment.kt
+++ wear/src/main/java/uk/co/zac_h/spacex/dashboard/DashboardWearFragment.kt
@@ -3,33 +3,20 @@ package uk.co.zac_h.spacex.dashboard
 import android.app.Fragment
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import kotlinx.android.synthetic.main.fragment_dashboard.*
-import kotlinx.coroutines.*
-import retrofit2.HttpException
-
 import uk.co.zac_h.spacex.R
 import uk.co.zac_h.spacex.utils.data.LaunchesModel
-import uk.co.zac_h.spacex.utils.data.rest.SpaceXInterface
 import uk.co.zac_h.spacex.utils.format
-import java.util.concurrent.TimeUnit
-import kotlin.coroutines.CoroutineContext
 
-class DashboardWearFragment : Fragment() {
+class DashboardWearFragment : Fragment(), DashboardWearView {
 
-    private val parentJob = Job()
-    private val coroutineContext: CoroutineContext
-        get() = parentJob + Dispatchers.Default
-
-    private val scope = CoroutineScope(coroutineContext)
+    private lateinit var presenter: DashboardWearPresenter
 
     private var countdown: CountDownTimer? = null
-
-    private var launch: LaunchesModel? = null
-    private var latest: LaunchesModel? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,125 +26,73 @@ class DashboardWearFragment : Fragment() {
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        dashboard_name_text.isSelected = true
+        presenter = DashboardWearPresenterImpl(this, DashboardWearInteractorImpl())
 
-        scope.launch {
-            val response = async(SupervisorJob(parentJob)) {
-                SpaceXInterface.create().getSingleLaunch("next")
-            }
-
-            withContext(Dispatchers.Main) {
-                try {
-                    if (response.await().isSuccessful) {
-                        launch = response.await().body()
-                        setCountdown()
-                        setData()
-                    } else {
-
-                    }
-                } catch (e: HttpException) {
-
-                } catch (e: Throwable) {
-                    Log.e(
-                        this@DashboardWearFragment.javaClass.name,
-                        e.localizedMessage ?: "Job failed to execute"
-                    )
-                }
-            }
+        presenter.apply {
+            getLaunch("next")
+            getLaunch("latest")
         }
-
-        scope.launch {
-            val response = async(SupervisorJob(parentJob)) {
-                SpaceXInterface.create().getSingleLaunch("latest")
-            }
-
-            withContext(Dispatchers.Main) {
-                try {
-                    if (response.await().isSuccessful) {
-                        latest = response.await().body()
-                        setLatest()
-                    } else {
-
-                    }
-                } catch (e: HttpException) {
-
-                } catch (e: Throwable) {
-                    Log.e(
-                        this@DashboardWearFragment.javaClass.name,
-                        e.localizedMessage ?: "Job failed to execute"
-                    )
-                }
-            }
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        setCountdown()
     }
 
     override fun onPause() {
         super.onPause()
         countdown?.cancel()
+        presenter.cancelRequests()
     }
 
-    private fun setData() {
-        launch?.let { model ->
-            dashboard_name_text.text = model.missionName
-            dashboard_date_text.text = launch?.tbd?.let {
-                model.launchDateUnix.format(it)
-            } ?: model.launchDateUnix.format()
-            dashboard_flight_text.text =
-                context?.getString(R.string.flight_number, model.flightNumber)
-        }
+    override fun updateNextLaunch(launch: LaunchesModel) {
+        dashboard_name_text.text = launch.missionName
+        dashboard_date_text.text = launch.tbd?.let {
+            launch.launchDateUnix.format(it)
+        } ?: launch.launchDateUnix.format()
+        dashboard_flight_text.text =
+            context?.getString(R.string.flight_number, launch.flightNumber)
     }
 
-    private fun setLatest() {
-        latest?.let { model ->
-            dashboard_latest_name_text.text = model.missionName
-            dashboard_latest_date_text.text = launch?.tbd?.let {
-                model.launchDateUnix.format(it)
-            } ?: model.launchDateUnix.format()
-            dashboard_latest_flight_text.text =
-                context?.getString(R.string.flight_number, model.flightNumber)
-        }
+    override fun updateLatestLaunch(launch: LaunchesModel) {
+        dashboard_latest_name_text.text = launch.missionName
+        dashboard_latest_date_text.text = launch.tbd?.let {
+            launch.launchDateUnix.format(it)
+        } ?: launch.launchDateUnix.format()
+        dashboard_latest_flight_text.text =
+            context?.getString(R.string.flight_number, launch.flightNumber)
     }
 
-    private fun setCountdown() {
-        launch?.let {
-            val time = it.launchDateUnix.times(1000) - System.currentTimeMillis()
+    override fun setCountdown(launchDateUnix: Long) {
+        val time = launchDateUnix.times(1000) - System.currentTimeMillis()
 
-            countdown = object : CountDownTimer(time, 1000) {
-                override fun onTick(millisUntilFinished: Long) {
-                    val format = String.format(
-                        "%02d:%02d:%02d:%02d",
-                        TimeUnit.MILLISECONDS.toDays(millisUntilFinished),
-                        TimeUnit.MILLISECONDS.toHours(millisUntilFinished) -
-                                TimeUnit.DAYS.toHours(
-                                    TimeUnit.MILLISECONDS.toDays(
-                                        millisUntilFinished
-                                    )
-                                ),
-                        TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) -
-                                TimeUnit.HOURS.toMinutes(
-                                    TimeUnit.MILLISECONDS.toHours(
-                                        millisUntilFinished
-                                    )
-                                ),
-                        TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) -
-                                TimeUnit.MINUTES.toSeconds(
-                                    TimeUnit.MILLISECONDS.toMinutes(
-                                        millisUntilFinished
-                                    )
-                                )
-                    )
-                    dashboard_countdown_text.text = format
-                }
+        countdown = object : CountDownTimer(time, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                presenter.updateCountdown(millisUntilFinished)
+            }
 
-                override fun onFinish() {
-                    dashboard_countdown_text.text = "Finished"
-                }
-            }.start()
-        }
+            override fun onFinish() {
+                dashboard_countdown_text.text = "Finished"
+            }
+        }.start()
+    }
+
+    override fun updateCountdown(countdown: String) {
+        dashboard_countdown_text.text = countdown
+    }
+
+    override fun showNextProgress() {
+        dashboard_next_progress_bar.visibility = View.VISIBLE
+    }
+
+    override fun showLatestProgress() {
+        dashboard_latest_progress_bar.visibility = View.VISIBLE
+    }
+
+    override fun hideNextProgress() {
+        dashboard_next_progress_bar.visibility = View.GONE
+    }
+
+    override fun hideLatestProgress() {
+        dashboard_latest_progress_bar.visibility = View.GONE
+    }
+
+    override fun showError(error: String) {
+        Toast.makeText(context, error, Toast.LENGTH_SHORT).show()
     }
 }
---
+++ wear/src/main/java/uk/co/zac_h/spacex/dashboard/DashboardWearPresenterImpl.kt
@@ -0,0 +1,69 @@
+package uk.co.zac_h.spacex.dashboard
+
+import uk.co.zac_h.spacex.utils.data.LaunchesModel
+import java.util.concurrent.TimeUnit
+
+class DashboardWearPresenterImpl(
+    private val view: DashboardWearView,
+    private val interactor: DashboardWearInteractor
+) : DashboardWearPresenter, DashboardWearInteractor.Callback {
+
+    override fun getLaunch(id: String) {
+        interactor.getSingleLaunch(id, this)
+        view.apply {
+            showNextProgress()
+            showLatestProgress()
+        }
+    }
+
+    override fun updateCountdown(time: Long) {
+        val remaining = String.format(
+            "%02d:%02d:%02d:%02d",
+            TimeUnit.MILLISECONDS.toDays(time),
+            TimeUnit.MILLISECONDS.toHours(time) - TimeUnit.DAYS.toHours(
+                TimeUnit.MILLISECONDS.toDays(
+                    time
+                )
+            ),
+            TimeUnit.MILLISECONDS.toMinutes(time) - TimeUnit.HOURS.toMinutes(
+                TimeUnit.MILLISECONDS.toHours(
+                    time
+                )
+            ),
+            TimeUnit.MILLISECONDS.toSeconds(time) - TimeUnit.MINUTES.toSeconds(
+                TimeUnit.MILLISECONDS.toMinutes(
+                    time
+                )
+            )
+        )
+
+        view.updateCountdown(remaining)
+    }
+
+    override fun cancelRequests() {
+        interactor.cancelAllRequests()
+    }
+
+    override fun onNextSuccess(launch: LaunchesModel?) {
+        launch?.let {
+            view.apply {
+                updateNextLaunch(it)
+                hideNextProgress()
+                setCountdown(it.launchDateUnix)
+            }
+        }
+    }
+
+    override fun onLatestSuccess(launch: LaunchesModel?) {
+        launch?.let {
+            view.apply{
+                updateLatestLaunch(it)
+                hideLatestProgress()
+            }
+        }
+    }
+
+    override fun onError(error: String) {
+        view.showError(error)
+    }
+}
