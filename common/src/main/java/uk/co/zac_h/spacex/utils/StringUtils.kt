e66ca9fccb8ecf82ac162f279af149859d355746
Zac Hadjineophytou <zac1999ad@gmail.com>
2019-10-04 13:49:55 +0100

    Removed MPAndroidChart library from common.

--- common/src/main/java/uk/co/zac_h/spacex/utils/StringUtils.kt
+++ common/src/main/java/uk/co/zac_h/spacex/utils/StringUtils.kt
@@ -1,16 +1,24 @@
 package uk.co.zac_h.spacex.utils
 
+import android.graphics.Color
 import android.graphics.Typeface
 import android.text.SpannableString
 import android.text.style.ForegroundColorSpan
 import android.text.style.RelativeSizeSpan
 import android.text.style.StyleSpan
-import com.github.mikephil.charting.utils.ColorTemplate
 
 fun String.generateCenterSpannableText(): SpannableString =
     SpannableString(this).apply {
         setSpan(RelativeSizeSpan(1.7f), 0, 8, 0)
         setSpan(StyleSpan(Typeface.NORMAL), 8, length - 11, 0)
         setSpan(RelativeSizeSpan(.8f), 8, length - 11, 0)
-        setSpan(ForegroundColorSpan(ColorTemplate.rgb("29b6f6")), length - 11, length, 0)
+        setSpan(ForegroundColorSpan("29b6f6".rgb()), length - 11, length, 0)
     }
+
+private fun String.rgb(): Int {
+    val color = java.lang.Long.parseLong(this.replace("#", ""), 16).toInt()
+    val r = color shr 16 and 0xFF
+    val g = color shr 8 and 0xFF
+    val b = color shr 0 and 0xFF
+    return Color.rgb(r, g, b)
+}
