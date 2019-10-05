abc7e63d8fa149f7e7997ba1609aabd82e7e2b7f
Zac Hadjineophytou <zac1999ad@gmail.com>
2019-10-05 13:45:31 +0100

    Added mission patch to launches layout. Dashboard launches are in a CardView.

--- app/src/main/java/uk/co/zac_h/spacex/dashboard/adapters/DashboardLaunchesAdapter.kt
+++ app/src/main/java/uk/co/zac_h/spacex/dashboard/adapters/DashboardLaunchesAdapter.kt
@@ -4,10 +4,13 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.core.os.bundleOf
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import uk.co.zac_h.spacex.R
 import uk.co.zac_h.spacex.model.LaunchesModel
 import uk.co.zac_h.spacex.utils.formatBlockNumber
@@ -43,6 +46,12 @@ class DashboardLaunchesAdapter(
             }
 
             launch?.let {
+                missionPatch.visibility =
+                    launch.links.missionPatchSmall?.let { View.VISIBLE } ?: View.GONE
+
+                Picasso.get().load(launch.links.missionPatchSmall)
+                    .into(missionPatch)
+
                 flightNumber.text = context?.getString(R.string.flight_number, it.flightNumber)
 
                 blockNumber.text = context?.getString(
@@ -55,7 +64,7 @@ class DashboardLaunchesAdapter(
                     it.launchDateUnix.formatDateMillisLong(tbd)
                 } ?: it.launchDateUnix.formatDateMillisLong()
 
-                itemView.setOnClickListener { _ ->
+                launchesCard.setOnClickListener { _ ->
                     itemView.findNavController().navigate(
                         R.id.action_dashboard_page_fragment_to_launch_details_fragment,
                         bundleOf("launch" to it, "title" to it.missionName)
@@ -68,10 +77,12 @@ class DashboardLaunchesAdapter(
     override fun getItemCount(): Int = launches.size
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val launchesCard: CardView = itemView.findViewById(R.id.launches_card_view)
+        val missionPatch: ImageView = itemView.findViewById(R.id.launches_mission_patch_image)
         val heading: TextView = itemView.findViewById(R.id.dashboard_heading_text)
-        val flightNumber: TextView = itemView.findViewById(R.id.dashboard_flight_no_text)
-        val blockNumber: TextView = itemView.findViewById(R.id.dashboard_block_text)
-        val missionName: TextView = itemView.findViewById(R.id.dashboard_mission_name_text)
-        val date: TextView = itemView.findViewById(R.id.dashboard_date_text)
+        val flightNumber: TextView = itemView.findViewById(R.id.launches_flight_no_text)
+        val blockNumber: TextView = itemView.findViewById(R.id.launches_block_text)
+        val missionName: TextView = itemView.findViewById(R.id.launches_mission_name_text)
+        val date: TextView = itemView.findViewById(R.id.launches_date_text)
     }
 }
