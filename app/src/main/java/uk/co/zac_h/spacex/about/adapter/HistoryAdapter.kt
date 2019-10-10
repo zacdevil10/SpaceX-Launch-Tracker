7099e7df9baa538033140893fe809f829f9a9bc4
Zac Hadjineophytou <zac1999ad@gmail.com>
2019-10-10 17:58:46 +0100

    Removed CardView layout from history events.

--- app/src/main/java/uk/co/zac_h/spacex/about/adapter/HistoryAdapter.kt
+++ app/src/main/java/uk/co/zac_h/spacex/about/adapter/HistoryAdapter.kt
@@ -1,11 +1,9 @@
 package uk.co.zac_h.spacex.about.adapter
 
-import android.animation.LayoutTransition
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.os.bundleOf
 import androidx.navigation.findNavController
@@ -16,8 +14,6 @@ import uk.co.zac_h.spacex.utils.formatDateMillisDDMMM
 
 class HistoryAdapter(private var events: ArrayList<HistoryHeaderModel>) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-
-    private var expandedPosition = -1
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
         when (viewType) {
@@ -46,14 +42,8 @@ class HistoryAdapter(private var events: ArrayList<HistoryHeaderModel>) :
 
         when (holder) {
             is ViewHolder -> holder.apply {
-                val isExpanded = position == expandedPosition
-
                 lineBottom.visibility =
                     if (position == events.size - 1) View.INVISIBLE else View.VISIBLE
-
-                details.visibility = if (isExpanded) View.VISIBLE else View.GONE
-
-                root.layoutTransition.disableTransitionType(LayoutTransition.CHANGING)
 
                 date.text = event.historyModel?.dateUnix?.formatDateMillisDDMMM()
                 title.text = event.historyModel?.title
@@ -73,12 +63,14 @@ class HistoryAdapter(private var events: ArrayList<HistoryHeaderModel>) :
 
                 launchDetailsButton.visibility =
                     event.historyModel?.flightNumber?.let { View.VISIBLE } ?: View.GONE
+                redditButton.visibility =
+                    event.historyModel?.links?.reddit?.let { View.VISIBLE } ?: View.GONE
+                wikiButton.visibility =
+                    event.historyModel?.links?.wikipedia?.let { View.VISIBLE } ?: View.GONE
+                articleButton.visibility =
+                    event.historyModel?.links?.article?.let { View.VISIBLE } ?: View.GONE
 
-                card.setOnClickListener {
-                    expandedPosition = if (isExpanded) -1 else position
-                    root.layoutTransition.enableTransitionType(LayoutTransition.CHANGING)
-                    notifyItemChanged(position)
-                }
+
             }
             is HeaderViewHolder -> holder.apply {
                 lineTop.visibility =
@@ -100,16 +92,19 @@ class HistoryAdapter(private var events: ArrayList<HistoryHeaderModel>) :
     override fun getItemViewType(position: Int): Int = if (events[position].isHeader) 0 else 1
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val card: CardView = itemView.findViewById(R.id.list_item_history_card)
         val date: TextView = itemView.findViewById(R.id.list_item_history_date)
         val title: TextView = itemView.findViewById(R.id.list_item_history_title)
         val details: TextView = itemView.findViewById(R.id.list_item_history_details)
-        val launchDetailsButton: TextView =
+        val launchDetailsButton: ConstraintLayout =
             itemView.findViewById(R.id.list_item_history_details_button)
+        val redditButton: ConstraintLayout =
+            itemView.findViewById(R.id.list_item_history_reddit_button)
+        val wikiButton: ConstraintLayout =
+            itemView.findViewById(R.id.list_item_history_wiki_button)
+        val articleButton: ConstraintLayout =
+            itemView.findViewById(R.id.list_item_history_article_button)
 
         val lineBottom: View = itemView.findViewById(R.id.list_item_history_line_bottom)
-
-        val root: ConstraintLayout = itemView.findViewById(R.id.root)
     }
 
     class HeaderViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
--- app/src/main/java/uk/co/zac_h/spacex/about/history/HistoryFragment.kt
+++ app/src/main/java/uk/co/zac_h/spacex/about/history/HistoryFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.SimpleItemAnimator
 import kotlinx.android.synthetic.main.fragment_history.*
 import uk.co.zac_h.spacex.R
 import uk.co.zac_h.spacex.about.adapter.HistoryAdapter
@@ -34,10 +33,9 @@ class HistoryFragment : Fragment(), HistoryView {
 
         history_recycler.apply {
             layoutManager = LinearLayoutManager(this@HistoryFragment.context)
-            setHasFixedSize(false)
+            setHasFixedSize(true)
             adapter = historyAdapter
             addItemDecoration(HeaderItemDecoration(this, historyAdapter.isHeader()))
-            (itemAnimator as SimpleItemAnimator).supportsChangeAnimations = false
         }
 
         presenter.getHistory()
