909bdeda17c4a361f6466089a5e88f79e64ca16a
Zac Hadjineophytou <zac1999ad@gmail.com>
2019-10-27 13:42:35 +0000

    Added example slice provider.

---
+++ app/src/main/java/uk/co/zac_h/spacex/utils/slices/SliceProvider.kt
@@ -0,0 +1,99 @@
+package uk.co.zac_h.spacex.utils.slices
+
+import android.app.PendingIntent
+import android.content.ContentResolver
+import android.content.Intent
+import android.net.Uri
+import androidx.core.graphics.drawable.IconCompat
+import androidx.slice.Slice
+import androidx.slice.SliceProvider
+import androidx.slice.builders.ListBuilder
+import androidx.slice.builders.SliceAction
+import androidx.slice.builders.list
+import androidx.slice.builders.row
+import uk.co.zac_h.spacex.R
+import uk.co.zac_h.spacex.base.MainActivity
+
+class SliceProvider : SliceProvider() {
+    /**
+     * Instantiate any required objects. Return true if the provider was successfully created,
+     * false otherwise.
+     */
+    override fun onCreateSliceProvider(): Boolean {
+        return true
+    }
+
+    /**
+     * Converts URL to content URI (i.e. content://uk.co.zac_h.spacex...)
+     */
+    override fun onMapIntentToUri(intent: Intent?): Uri {
+        // Note: implementing this is only required if you plan on catching URL requests.
+        // This is an example solution.
+        var uriBuilder: Uri.Builder = Uri.Builder().scheme(ContentResolver.SCHEME_CONTENT)
+        if (intent == null) return uriBuilder.build()
+        val data = intent.data
+        val dataPath = data?.path
+        if (data != null && dataPath != null) {
+            val path = dataPath.replace("/next", "")
+            uriBuilder = uriBuilder.path(path)
+        }
+        val context = context
+        if (context != null) {
+            uriBuilder = uriBuilder.authority(context.packageName)
+        }
+        return uriBuilder.build()
+    }
+
+    /**
+     * Construct the Slice and bind data if available.
+     */
+    override fun onBindSlice(sliceUri: Uri): Slice? {
+        // Note: you should switch your build.gradle dependency to
+        // slice-builders-ktx for a nicer interface in Kotlin.
+        val context = context ?: return null
+        val activityAction = createActivityAction() ?: return null
+        return if (sliceUri.path == "/next") {
+            // Path recognized. Customize the Slice using the androidx.slice.builders API.
+            // Note: ANR and StrictMode are enforced here so don't do any heavy operations. 
+            // Only bind data that is currently available in memory.
+            list(context, sliceUri, ListBuilder.INFINITY) {
+                row {
+                    primaryAction = activityAction
+                    title = "Next Launch."
+                }
+            }
+        } else {
+            // Error: Path not found.
+            list(context, sliceUri, ListBuilder.INFINITY) {
+                row {
+                    primaryAction = activityAction
+                    title = "URI not recognized."
+                }
+            }
+        }
+    }
+
+    private fun createActivityAction(): SliceAction? {
+        return SliceAction.create(
+            PendingIntent.getActivity(context, 0, Intent(context, MainActivity::class.java), 0),
+            IconCompat.createWithResource(context, R.mipmap.ic_launcher),
+            ListBuilder.ICON_IMAGE,
+            "Enter app"
+        )
+    }
+
+    /**
+     * Slice has been pinned to external process. Subscribe to data source if necessary.
+     */
+    override fun onSlicePinned(sliceUri: Uri?) {
+        // When data is received, call context.contentResolver.notifyChange(sliceUri, null) to
+        // trigger SliceProvider#onBindSlice(Uri) again.
+    }
+
+    /**
+     * Unsubscribe from data source if necessary.
+     */
+    override fun onSliceUnpinned(sliceUri: Uri?) {
+        // Remove any observers if necessary to avoid memory leaks.
+    }
+}
