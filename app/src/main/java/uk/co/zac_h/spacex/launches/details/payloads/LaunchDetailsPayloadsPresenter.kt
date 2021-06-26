2713395afa2315b334bf402cf7b1db21807d2612
Zac Hadjineophytou <zac@zac-h.co.uk>
2021-06-26 16:00:46 +0100

    Refactored Launch Payloads

--- app/src/main/java/uk/co/zac_h/spacex/launches/details/payloads/LaunchDetailsPayloadsPresenter.kt
+++ app/src/main/java/uk/co/zac_h/spacex/launches/details/payloads/LaunchDetailsPayloadsPresenter.kt
@@ -1,15 +1,14 @@
 package uk.co.zac_h.spacex.launches.details.payloads
 
 import uk.co.zac_h.spacex.base.NetworkInterface
-import uk.co.zac_h.spacex.model.spacex.Launch
 import uk.co.zac_h.spacex.model.spacex.Payload
 import uk.co.zac_h.spacex.rest.SpaceXInterface
 import uk.co.zac_h.spacex.utils.SPACEX_BASE_URL_V5
 
 class LaunchDetailsPayloadsPresenter(
     private val view: NetworkInterface.View<List<Payload>>,
-    private val interactor: NetworkInterface.Interactor<Launch?>
-) : NetworkInterface.Presenter<Nothing>, NetworkInterface.Callback<Launch?> {
+    private val interactor: NetworkInterface.Interactor<List<Payload>>
+) : NetworkInterface.Presenter<Nothing>, NetworkInterface.Callback<List<Payload>> {
 
     override fun get(data: Any, api: SpaceXInterface) {
         view.toggleSwipeRefresh(true)
@@ -20,12 +19,11 @@ class LaunchDetailsPayloadsPresenter(
         interactor.cancelAllRequests()
     }
 
-    override fun onSuccess(response: Launch?) {
-        response?.payloads?.let { payloads ->
-            view.update(payloads)
-            view.toggleSwipeRefresh(false)
+    override fun onSuccess(response: List<Payload>) {
+        view.apply {
+            update(response)
+            toggleSwipeRefresh(false)
         }
-
     }
 
     override fun onError(error: String) {
