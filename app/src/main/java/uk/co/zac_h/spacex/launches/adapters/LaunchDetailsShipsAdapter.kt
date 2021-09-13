04120b0e6adec653097216aa0614b25311e8fdef
Zac Hadjineophytou <zac@zac-h.co.uk>
2021-09-13 14:45:47 +0100

    Fixed crew details not showing when opened from launch details

--- app/src/main/java/uk/co/zac_h/spacex/launches/adapters/LaunchDetailsShipsAdapter.kt
+++ app/src/main/java/uk/co/zac_h/spacex/launches/adapters/LaunchDetailsShipsAdapter.kt
@@ -3,7 +3,6 @@ package uk.co.zac_h.spacex.launches.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.os.bundleOf
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.recyclerview.widget.RecyclerView
@@ -11,6 +10,7 @@ import com.bumptech.glide.Glide
 import uk.co.zac_h.spacex.R
 import uk.co.zac_h.spacex.databinding.ListItemVehicleBinding
 import uk.co.zac_h.spacex.dto.spacex.Ship
+import uk.co.zac_h.spacex.launches.details.LaunchDetailsContainerFragmentDirections
 
 class LaunchDetailsShipsAdapter : RecyclerView.Adapter<LaunchDetailsShipsAdapter.ViewHolder>() {
 
@@ -50,9 +50,9 @@ class LaunchDetailsShipsAdapter : RecyclerView.Adapter<LaunchDetailsShipsAdapter
     class ViewHolder(val binding: ListItemVehicleBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(ship: Ship) {
             binding.root.findNavController().navigate(
-                R.id.action_launch_details_container_fragment_to_ship_details_fragment,
-                bundleOf("ship" to ship),
-                null,
+                LaunchDetailsContainerFragmentDirections.actionLaunchDetailsContainerFragmentToShipDetailsFragment(
+                    ship.name
+                ),
                 FragmentNavigatorExtras(binding.vehicleCard to ship.id)
             )
         }
