1f606d625a69bba5b2704e96c3fbb14217a99e92
Zac Hadjineophytou <zac@zac-h.co.uk>
2021-11-05 14:50:31 +0000

    Moved enum to common

--- app/src/main/java/uk/co/zac_h/spacex/launches/FlowTypeViewModel.kt
+++ app/src/main/java/uk/co/zac_h/spacex/launches/FlowTypeViewModel.kt
@@ -2,6 +2,7 @@ package uk.co.zac_h.spacex.launches
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import uk.co.zac_h.spacex.types.LaunchType
 import javax.inject.Inject
 
 @HiltViewModel
---
+++ common/src/main/java/uk/co/zac_h/spacex/types/LaunchType.kt
@@ -0,0 +1,5 @@
+package uk.co.zac_h.spacex.types
+
+enum class LaunchType(val typeString: String) {
+    UPCOMING("upcoming"), PAST("past")
+}
