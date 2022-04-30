2ff32556263c2402b344b3a9d7ae569bc0e4dd81
Zac Hadjineophytou <zac@zac-h.co.uk>
2022-04-30 16:18:29 +0100

    Removed unused animations

--- app/src/main/java/uk/co/zac_h/spacex/utils/AnimationUtils.kt
+++ app/src/main/java/uk/co/zac_h/spacex/utils/AnimationUtils.kt
@@ -6,12 +6,6 @@ import android.view.animation.AnimationUtils
 import android.view.animation.LayoutAnimationController
 import uk.co.zac_h.spacex.R
 
-fun animateFromRightWithOffset(context: Context?, offset: Long): Animation =
-    AnimationUtils.loadAnimation(
-        context,
-        R.anim.item_animation_from_right
-    ).apply { startOffset = offset }
-
 fun animateEnterFromTop(context: Context?): Animation =
     AnimationUtils.loadAnimation(context, R.anim.slide_in_top)
 
@@ -23,12 +17,3 @@ fun animateLayoutFromBottom(context: Context?): LayoutAnimationController =
         context,
         R.anim.layout_animation_from_bottom
     )
-
-fun animateFadeIn(context: Context?): Animation =
-    AnimationUtils.loadAnimation(context, R.anim.fade_in)
-
-fun animateFadeOut(context: Context?): Animation =
-    AnimationUtils.loadAnimation(context, R.anim.fade_out)
-
-fun animationScaleUpWithOffset(context: Context?): Animation =
-    AnimationUtils.loadAnimation(context, R.anim.scale_up)
