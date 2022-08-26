6d2865ec6edf0db3598b5525db23c2256e2d675b
Zac Hadjineophytou <zac@zac-h.co.uk>
2022-08-26 14:01:42 +0100

    Removed filter from reddit feed

--- app/src/main/java/uk/co/zac_h/spacex/news/reddit/RedditFeedRepository.kt
+++ app/src/main/java/uk/co/zac_h/spacex/news/reddit/RedditFeedRepository.kt
@@ -1,6 +1,5 @@
 package uk.co.zac_h.spacex.news.reddit
 
-import uk.co.zac_h.spacex.REDDIT_PARAM_ORDER_HOT
 import uk.co.zac_h.spacex.datasource.remote.news.RedditPagingSource
 import uk.co.zac_h.spacex.retrofit.RedditHttpClient
 import uk.co.zac_h.spacex.retrofit.RedditService
@@ -10,11 +9,6 @@ class RedditFeedRepository @Inject constructor(
     @RedditHttpClient private val httpService: RedditService
 ) {
 
-    var order = REDDIT_PARAM_ORDER_HOT
-
     val redditPagingSource: RedditPagingSource
-        get() = RedditPagingSource(httpService).also {
-            it.order = order
-        }
-
+        get() = RedditPagingSource(httpService)
 }
