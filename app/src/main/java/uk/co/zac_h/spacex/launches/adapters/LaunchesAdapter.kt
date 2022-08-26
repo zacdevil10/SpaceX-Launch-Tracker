e268e7bd6a162c5b6165d04f82c3c81203d2b0b9
Zac Hadjineophytou <zac@zac-h.co.uk>
2022-08-26 17:19:53 +0100

    Added widget module

--- app/src/main/java/uk/co/zac_h/spacex/launches/adapters/LaunchesAdapter.kt
+++ app/src/main/java/uk/co/zac_h/spacex/launches/adapters/LaunchesAdapter.kt
@@ -1,24 +1,18 @@
 package uk.co.zac_h.spacex.launches.adapters
 
-import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import uk.co.zac_h.spacex.NavGraphDirections
-import uk.co.zac_h.spacex.R
 import uk.co.zac_h.spacex.databinding.ListItemLaunchesBinding
 import uk.co.zac_h.spacex.launches.Launch
 import uk.co.zac_h.spacex.utils.formatDateMillisLong
 
-class LaunchesAdapter(private val context: Context) :
-    ListAdapter<Launch, LaunchesAdapter.ViewHolder>(Comparator) {
+class LaunchesAdapter : ListAdapter<Launch, LaunchesAdapter.ViewHolder>(Comparator) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder = ViewHolder(
         ListItemLaunchesBinding.inflate(
@@ -31,33 +25,24 @@ class LaunchesAdapter(private val context: Context) :
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val launch = getItem(position)
 
-        holder.binding.apply {
+        with(holder.binding) {
             root.transitionName = launch.id
 
-            Glide.with(root)
-                .load(launch.links?.missionPatch?.patchSmall)
-                .error(ContextCompat.getDrawable(context, R.drawable.ic_mission_patch))
-                .fallback(ContextCompat.getDrawable(context, R.drawable.ic_mission_patch))
-                .placeholder(ContextCompat.getDrawable(context, R.drawable.ic_mission_patch))
-                .into(missionPatch)
+            launchView.apply {
+                patch = launch.links?.missionPatch?.patchSmall
+                flightNumber = launch.flightNumber
+                vehicle = launch.rocket?.name
+                missionName = launch.missionName
+                date = launch.launchDate?.dateUnix?.formatDateMillisLong(launch.datePrecision)
 
-            if (launch.rocket?.name == "Falcon 9") {
-                reused.visibility =
-                    if (launch.cores?.first()?.reused == true) View.VISIBLE else View.GONE
-
-                landingVehicle.visibility =
-                    if (launch.cores?.first()?.landingSuccess == true) View.VISIBLE else View.GONE
-
-                landingVehicle.text = launch.cores?.first()?.landingPad?.name
-            } else {
-                reused.visibility = View.GONE
-                landingVehicle.visibility = View.GONE
+                if (launch.rocket?.name == "Falcon 9") {
+                    isReused = launch.cores?.first()?.reused ?: false
+                    landingPad = launch.cores?.first()?.landingPad?.name
+                } else {
+                    isReused = false
+                    landingPad = null
+                }
             }
-
-            flightNumber.text = context.getString(R.string.flight_number, launch.flightNumber)
-            vehicle.text = launch.rocket?.name
-            missionName.text = launch.missionName
-            date.text = launch.launchDate?.dateUnix?.formatDateMillisLong(launch.datePrecision)
 
             root.setOnClickListener {
                 root.findNavController().navigate(
