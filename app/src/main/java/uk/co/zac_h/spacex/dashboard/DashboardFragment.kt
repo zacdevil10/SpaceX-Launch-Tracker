afe94c5f8d7e99846a8728c3eb3fc9c58fbf8da1
Zac Hadjineophytou <zac@zac-h.co.uk>
2022-10-14 17:53:58 +0100

    Updated dashboard layout

--- app/src/main/java/uk/co/zac_h/spacex/dashboard/DashboardFragment.kt
+++ app/src/main/java/uk/co/zac_h/spacex/dashboard/DashboardFragment.kt
@@ -123,7 +123,7 @@ class DashboardFragment : BaseFragment() {
                     pinnedAdapter.submitList(response.data)
                     binding.pinned.progress.hide()
                     binding.pinned.pinnedMessage.visibility =
-                        if (response.data.isNullOrEmpty()) View.VISIBLE else View.GONE
+                        if (response.data.isNullOrEmpty()) View.VISIBLE else View.INVISIBLE
                 }
                 is ApiResult.Failure -> showError(response.exception.message)
             }
