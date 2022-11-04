faf9ba29d6a4c1d2b050020f93d6bc8e64d4197e
Zac Hadjineophytou <zac@zac-h.co.uk>
2022-11-04 14:50:47 +0000

    Updated launch details layout

--- app/src/main/java/uk/co/zac_h/spacex/launches/details/details/LaunchDetailsFragment.kt
+++ app/src/main/java/uk/co/zac_h/spacex/launches/details/details/LaunchDetailsFragment.kt
@@ -62,21 +62,26 @@ class LaunchDetailsFragment : BaseFragment() {
                     }
                 }
 
+                holdCard.isVisible = !launch.holdReason.isNullOrEmpty()
+                holdDescription.text = launch.holdReason
+
+                missionCard.isVisible = !launch.description.isNullOrEmpty()
+                        || !launch.type.isNullOrEmpty()
+                        || !launch.orbit.isNullOrEmpty()
                 missionDescription.apply {
                     isVisible = !launch.description.isNullOrEmpty()
                     text = launch.description
                 }
-                typeText.text = launch.type
-                orbitText.text = launch.orbit
-
-                launchSiteCard.isVisible = launch.launchLocation != null
-                launchSiteText.text = launch.launchLocation
-                Glide.with(requireContext()).load(launch.launchLocationMap).into(launchSiteImage)
-                launchSiteImage.setOnClickListener { openWebLink(launch.launchLocationMapUrl) }
-
-                statusCard.isVisible = launch.status != null && launch.statusDescription != null
-                statusText.text = launch.status
-                statusDescription.text = launch.statusDescription
+                typeLabel.isVisible = !launch.type.isNullOrEmpty()
+                typeText.apply {
+                    isVisible = !launch.type.isNullOrEmpty()
+                    text = launch.type
+                }
+                orbitLabel.isVisible = !launch.orbit.isNullOrEmpty()
+                orbitText.apply {
+                    isVisible = !launch.orbit.isNullOrEmpty()
+                    text = launch.orbit
+                }
 
                 launchDetailsWatchButton.apply {
                     isVisible = launch.webcast != null
@@ -84,9 +89,19 @@ class LaunchDetailsFragment : BaseFragment() {
                 }
 
                 launchDetailsCalendarButton.apply {
-                    isVisible = launch.upcoming
+                    isVisible = launch.upcoming && !launch.webcastLive
                     setOnClickListener { createEvent(launch) }
                 }
+
+                statusCard.isVisible = launch.status.type != null
+                statusText.text = launch.status.name
+                statusDescription.text =
+                    if (launch.status.type == 4) launch.failReason else launch.status.description
+
+                launchSiteCard.isVisible = launch.launchLocation != null
+                launchSiteText.text = launch.launchLocation
+                Glide.with(requireContext()).load(launch.launchLocationMap).into(launchSiteImage)
+                launchSiteImage.setOnClickListener { openWebLink(launch.launchLocationMapUrl) }
             }
         }
     }
