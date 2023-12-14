753c3d6a73a19c07ebbbc588918cc6e9a4dfa640
Zac Hadjineophytou <zac@zac-h.co.uk>
2023-12-14 18:23:44 +0000

    Migrated VehicleView to compose

--- core/ui/src/main/java/uk/co/zac_h/spacex/core/ui/VehicleView.kt
+++ core/ui/src/main/java/uk/co/zac_h/spacex/core/ui/VehicleView.kt
@@ -1,70 +1,102 @@
 package uk.co.zac_h.spacex.core.ui
 
-import android.content.Context
-import android.util.AttributeSet
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.core.content.res.use
-import androidx.core.view.isVisible
-import com.bumptech.glide.Glide
-import com.google.android.material.button.MaterialButton
-import com.google.android.material.card.MaterialCardView
-import uk.co.zac_h.spacex.core.ui.databinding.VehicleViewBinding
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 
-class VehicleView @JvmOverloads constructor(
-    context: Context,
-    attrs: AttributeSet? = null
-) : MaterialCardView(context, attrs) {
-
-    private val binding: VehicleViewBinding =
-        VehicleViewBinding.inflate(LayoutInflater.from(context), this, true)
-
-    var image: String? = null
-        set(value) {
-            field = value
-
-            binding.vehicleImage.isVisible = value != null
-
-            Glide.with(this)
-                .load(value)
-                .error(R.drawable.ic_baseline_error_outline_24)
-                .into(binding.vehicleImage)
-        }
-
-    var title: String? = null
-        set(value) {
-            field = value
-
-            binding.vehicleName.text = value
-        }
-
-    var status: String? = null
-        set(value) {
-            field = value
-
-            binding.status.text = value
-        }
-
-    var buttonText: String? = null
-        set(value) {
-            field = value
-
-            binding.vehicleSpecs.text = value
-        }
-
-    val vehicleSpecs: MaterialButton
-        get() = binding.vehicleSpecs
-
-    init {
-        context.obtainStyledAttributes(attrs, R.styleable.VehicleView).use {
-            buttonText = it.getString(R.styleable.VehicleView_buttonText)
+@Composable
+fun VehicleView(
+    modifier: Modifier = Modifier,
+    image: String? = null,
+    title: String? = null,
+    status: String? = null,
+    buttonText: String? = null,
+    navigate: () -> Unit,
+    content: @Composable() (() -> Unit)? = null
+) {
+    ElevatedCard(
+        modifier = modifier
+            .clickable {
+                navigate()
+            },
+        shape = CardDefaults.elevatedShape
+    ) {
+        Column {
+            image?.let {
+                AsyncImage(
+                    model = it,
+                    contentDescription = "",
+                    placeholder = painterResource(id = R.drawable.ic_baseline_error_outline_24),
+                    error = painterResource(id = R.drawable.ic_baseline_error_outline_24),
+                    modifier = Modifier
+                        .height(256.dp)
+                        .fillMaxWidth()
+                        .clip(CardDefaults.elevatedShape),
+                    contentScale = ContentScale.Crop
+                )
+            }
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, top = 16.dp, end = 16.dp)
+            ) {
+                Text(text = title.orEmpty(), style = MaterialTheme.typography.headlineSmall)
+                Text(text = status.orEmpty(), modifier = Modifier.align(Alignment.CenterVertically))
+            }
+            content?.invoke()
+            Button(
+                onClick = {
+                    navigate()
+                },
+                modifier = Modifier
+                    .align(Alignment.End)
+                    .padding(top = 8.dp, end = 16.dp, bottom = 8.dp)
+            ) {
+                Text(text = buttonText.orEmpty())
+            }
         }
     }
+}
 
-    override fun addView(child: View?, params: ViewGroup.LayoutParams?) {
-        child?.let {
-            binding.body.addView(child, params)
-        } ?: super.addView(null, params)
+@Preview
+@Composable
+fun VehicleViewPreview(
+    @PreviewParameter(LoremIpsum::class) text: String
+) {
+    SpaceXTheme {
+        VehicleView(
+            image = "",
+            title = "Falcon 1",
+            status = "Status",
+            buttonText = "Specs",
+            navigate = {}
+        ) {
+            Text(
+                text = text,
+                maxLines = 4,
+                modifier = Modifier.padding(start = 16.dp, top = 8.dp, end = 24.dp)
+            )
+        }
     }
 }
--- feature/assets/src/main/java/uk/co/zac_h/spacex/feature/assets/vehicles/adapters/VehiclesAdapter.kt
+++ feature/assets/src/main/java/uk/co/zac_h/spacex/feature/assets/vehicles/adapters/VehiclesAdapter.kt
@@ -2,10 +2,18 @@ package uk.co.zac_h.spacex.feature.assets.vehicles.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import uk.co.zac_h.spacex.core.ui.SpaceXTheme
+import uk.co.zac_h.spacex.core.ui.VehicleView
+import uk.co.zac_h.spacex.feature.assets.R
 import uk.co.zac_h.spacex.feature.assets.databinding.ListItemVehicleBinding
 import uk.co.zac_h.spacex.feature.assets.vehicles.VehicleItem
 import uk.co.zac_h.spacex.feature.assets.vehicles.VehiclesFragmentDirections
@@ -24,21 +32,26 @@ class VehiclesAdapter(val setSelected: (VehicleItem) -> Unit) :
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val vehicle = getItem(position)
 
-        with(holder.binding) {
-            vehicleView.apply {
-                image = vehicle.imageUrl
-                title = vehicle.title
-                vehicleSpecs.setOnClickListener {
-                    setSelected(vehicle)
-                    holder.bind()
-                }
-                setOnClickListener {
-                    setSelected(vehicle)
-                    holder.bind()
+        holder.binding.vehicleView.setContent {
+            SpaceXTheme {
+                VehicleView(
+                    modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+                    image = vehicle.imageUrl,
+                    title = vehicle.title,
+                    buttonText = stringResource(R.string.vehicle_list_item_specs_button_label),
+                    navigate = {
+                        setSelected(vehicle)
+                        holder.bind()
+                    }
+                ) {
+                    vehicle.description?.let {
+                        Text(
+                            text = it,
+                            modifier = Modifier.padding(start = 16.dp, top = 8.dp, end = 24.dp)
+                        )
+                    }
                 }
             }
-
-            vehicleDetails.text = vehicle.description
         }
     }
 
